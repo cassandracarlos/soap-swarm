02d376754bca431248cce70ff8e47d607b8c8cb9
Cass <codingcass@gmail.com>
2024-02-12 15:09:47 -0800

    IT WORKS!
    He he

--- scripts/player_character.gd
+++ scripts/player_character.gd
@@ -1,14 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 500.0
-
-func _physics_process(delta):
-
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("up", "down")
-	if direction:
-		velocity.y = direction * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-
+func _physics_process(delta: float) -> void:
+	var vel : Vector2 = get_global_mouse_position() - global_position
+	velocity = vel
 	move_and_slide()
