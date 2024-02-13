e4836e059dc9111cc5cbfd7e4162a98b22e9ed02
Cass <codingcass@gmail.com>
2024-02-12 16:39:57 -0800

    Update pause_menu.gd

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -8,7 +8,7 @@ func pause():
 
 func testEsc():
 	
-	if Input.is_action_just_pressed("esc") and get_tree().paused == false:
+	if Input.is_action_just_pressed("esc") and !get_tree().paused == false:
 		pause()
 		
 	elif Input.is_action_just_pressed("esc") and get_tree().paused:
@@ -19,3 +19,6 @@ func _on_continue_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/start_menu.tscn")
+	
+func _process(delta):
+	testEsc()
