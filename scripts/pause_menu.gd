39b8920918850dd150f17b52cf131ac45de490ca
Cass <codingcass@gmail.com>
2024-02-12 16:29:02 -0800

    Pause menu update
    doesn't work yet

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,19 +1,21 @@
-extends Control
+extends Node
 
 func resume():
 	get_tree().paused = false
 
 func pause():
 	get_tree().paused = true
-	
+
 func testEsc():
-	if Input.is_action_just_pressed("esc") and !get_tree().paused:
+	
+	if Input.is_action_just_pressed("esc") and get_tree().paused == false:
 		pause()
+		
 	elif Input.is_action_just_pressed("esc") and get_tree().paused:
 		resume()
 
-func _on_resume_pressed():
+func _on_continue_button_pressed():
 	resume()
-	
-func _on_exit_button_pressed():
-	get_tree().quit()
+
+func _on_quit_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/start_menu.tscn")
